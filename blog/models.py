1c362ddfafe0669e9910bd3edbd63773581faa59
goback <test850317@gmail.com>
2021-07-30 23:07:19 +0000

    카테고리 목록 / 게시글 목록 link 연결  - Category / Post model 에 get_absolute_url 선언  - html 에 url 사용 예시 작성

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
-from django.db import models
+from django.db        import models
+from django.shortcuts import resolve_url
 
 class Post(models.Model):
     title       = models.CharField(max_length=30)
@@ -13,12 +14,18 @@ class Post(models.Model):
     def __str__(self):
         return f'[{self.pk}]{self.title}'
 
+    def get_absolute_url(self):
+        return resolve_url('blog:post_detail', self.pk)
+
 class Category(models.Model):
     name = models.CharField(max_length=45, unique=True)
 
     class Meta:
         db_table            = 'categories'
-        verbose_name_plural = "categories"
-    
+        verbose_name_plural = 'categories'
+
     def __str__(self):
         return f'[{self.pk}]{self.name}'
+
+    def get_absolute_url(self):
+        return resolve_url('blog:post_list', self.pk)
